549b4306ff25551051acbb4e1606e9ddb4932a4b
Ahmed Hafez <41956720+Ahmed-Hafez@users.noreply.github.com>
2020-08-21 23:37:27 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
+import { ProductDetailsGuard } from './product/product-details.guard';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
+import { RouterModule } from "@angular/router";
 
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { ProductListComponent } from './product/product-list.component';
 import { StarComponent } from './shared/star.component';
 import { AppComponent } from './app.component';
 import { ProductDetailsComponent } from './product/product-details/product-details.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +18,24 @@ import { ProductDetailsComponent } from './product/product-details/product-detai
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { 
+        path: 'products/:id',
+        canActivate: [ProductDetailsGuard],
+        component:ProductListComponent
+       },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full'} // Here we should redirect to page not found but I make this as demo version 
+    ])
   ],
   bootstrap: [AppComponent]
 })
--- src/app/product/product-details/product-details.component.ts
+++ src/app/product/product-details/product-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
 import { IProduct } from './../product';
 
 @Component({
@@ -11,9 +12,26 @@ export class ProductDetailsComponent implements OnInit {
   pageTitle: string = 'Product Details';
   product:IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += ": " + id;
+    this.product = {
+      'productId': id,
+      'productName': 'the name',
+      'productCode': 'dsa',
+      'description': 'dsadfsa',
+      'imageUrl': 'assets/images/leaf_rake.png',
+      'price': 19.95,
+      'starRating': 3.2,
+      'releaseDate': 'asdsadfs'
+    }
+  }
+
+  onBack(): void{
+    this.router.navigate(['/products']);
   }
 
 }
